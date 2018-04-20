af50f17763d05a05244103755b55417b242bdbea
kgkrudev34 <ru.dev34@trezub.ru>
2018-04-20 08:33:22 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+var userList = [
+	{'id': 100, 'username': 'Иванов Иван'},
+	{'id': 200, 'username': 'Семенов Семен'},
+]
+
+var dialogListArray = [
+	{	
+		'id': 4000,
+		'user_id': 100,
+		'last_message': 'Привет, это последнее сообщение'
+	},
+	{	
+		'id': 2000,
+		'user_id': 200,
+		'last_message': 'Привет, это последнее сообщение'
+	}
+]
+
+var messageArray = [
+		{'text': 'hello', 'sender': 100},
+		{'text': 'wassup?', 'sender': 200}
+	]
+
